43768e99b08f2041b388cf10cbf5824383199fce
jdquinonesmo <78066646+jdquinonesmo@users.noreply.github.com>
2023-07-13 17:02:23 -0500

    Update 04-Tarea 11 Examen 1.R

--- scripts/tema1/04-Tarea 11 Examen 1.R
+++ scripts/tema1/04-Tarea 11 Examen 1.R
@@ -1,6 +1,7 @@
 ##### EXAMEN 1 #####
 library(tidyverse)
 library(nycflights13)
+library(cowplot)
 # PUNTO 1.) #
 
   
@@ -10,7 +11,7 @@ library(nycflights13)
   filter(!is.na(dep_delay), !is.na(arr_delay))%>%
   group_by(carrier)%>%
   summarise(N=n(), n=sum(dep_delay>0), prop=n/N)
-  arrange(t1, prop)
+  arrange(t1, desc(prop))
   
 #b) Destino con mayor proporción de vuelos retrasados.
   
@@ -53,12 +54,47 @@ library(nycflights13)
   
 # PUNTO 4.) 
    
+  # Investiga si existe algún patrón del número de vuelos que se cancelan cada día.
   View(flights) 
   t4 <- flights %>% group_by(day) %>% summarise(n=sum(is.na(arr_time)), N=n(), p=n/N)
-  ggplot(t4, mapping = aes(x = day, y = p))+
+   ggplot(t4, mapping = aes(x = day, y = N))+
     geom_point()
-  ggplot(t4, mapping = aes(x = day, y = N))+
+  a<-ggplot(t4, mapping = aes(x = day, y = n))+
+  geom_point()
+  b<-ggplot(t4, mapping = aes(x = N, y= n))+
     geom_point()
-  ggplot(t4, mapping = aes(x = day, y = n))+
-    geom_point()
+  plot_grid(a, b)
+
+ t6 <- group_by(flights, carrier, day) %>% summarise(n=sum(is.na(arr_time)), N=n(), p=n/N)
+ a<-ggplot(t6, mapping = aes(x = day, y = p, col=carrier))+
+   geom_smooth(se=F)
+ arrange(t6, N)
+ View(filter(flights, carrier == "OO") %>% group_by(day)
+      %>% summarise(n=sum(is.na(arr_time)), N=n(), p=n/N))
+ t6 <- filter(t6, carrier != "OO")
+ b<-ggplot(t6, mapping = aes(x = day, y = p, col=carrier))+
+   geom_smooth(se=F)
+ plot_grid(a, b)
+ filter(flights, carrier == "OO") %>% summarise(n())
  
+ 
+ # Investiga si la proporción de vuelos cancelados está relacionada con el 
+ # retraso promedio por día en los vuelos.
+ View(flights)
+ t7 <- filter(flights, carrier != "OO")
+ t7 <- group_by(t7, year, month, day)  %>%
+   summarise(n=sum(is.na(arr_time)), N=n(), p=n/N, delay_prom=mean(dep_delay, na.rm=T))
+ ggplot(t7, mapping = aes(x = delay_prom, y = p))+
+   geom_smooth(se=F)+
+   geom_point()
+
+ #Investiga si la proporción de vuelos cancelados está relacionada con el 
+ #retraso promedio por aeropuerto en los vuelos.
+ 
+ t8 <- filter(flights, carrier != "OO")
+ t8 <- group_by(t8, origin)  %>%
+   summarise(n=sum(is.na(arr_time)), N=n(), p=n/N, delay_prom=mean(dep_delay, na.rm=T))
+ ggplot(t8, mapping = aes(x = delay_prom, y = p))+
+    geom_text(aes(label=origin))
+
+ sum(is.na(flights$arr_time))
